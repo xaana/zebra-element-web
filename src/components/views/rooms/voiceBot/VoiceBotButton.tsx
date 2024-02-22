d892e67dda04621ffbc8d30684e0c697d0eda712
XinjieJayWang <jay.wang@xaana.ai>
2024-02-22 18:09:18 +1100

    update CSP and audio process script for silence detection

--- src/components/views/rooms/voiceBot/VoiceBotButton.tsx
+++ src/components/views/rooms/voiceBot/VoiceBotButton.tsx
@@ -91,47 +91,46 @@ export const voiceBotState = create<State & Action>((set) => ({
 
 const workletCode = `
 export default class MyAudioWorkletProcessor extends AudioWorkletProcessor {
-    public average: number = 0;
-    public count: number = 0;
-    public silenceThreshold: number = 2;
-    public silenceCountThreshold: number = 3;
-    // sampleRate: number = 44100;
-    public currentFrame:number = 0
-    public _volume:number =0
-  
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    public process(inputs: Float32Array[][]) {
-      const input = inputs[0];
-      const channel = input[0];
+  average = 0;
+  count = 0;
+  silenceThreshold = 2;
+  silenceCountThreshold = 3;
+  // sampleRate: number = 44100;
+  currentFrame = 0
+  _volume =0
 
-      this.currentFrame+=1
-      if (this.currentFrame % 200 === 0) {
-        let sum = 0;
-        for (let i = 0; i < channel.length; i++) {
-          sum += Math.abs(channel[i]);
-        }
-        // console.log(sum)
-        this.average = sum *1000/ channel.length;
-        if (this.average<this.silenceThreshold){
-            this.count+=1
-        }
-        else{
-            this.count=0
-        }
-        
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  process(inputs) {
+    const input = inputs[0];
+    const channel = input[0];
+
+    this.currentFrame+=1
+    if (this.currentFrame % 200 === 0) {
+      let sum = 0;
+      for (let i = 0; i < channel.length; i++) {
+        sum += Math.abs(channel[i]);
       }
-      console.log(this.average)
-      if (this.count > this.silenceCountThreshold) {
-        this.port.postMessage('silence-detected');
-        return false; // Stop processing audio when silence is detected
+      // console.log(sum)
+      this.average = sum *1000/ channel.length;
+      if (this.average<this.silenceThreshold){
+          this.count+=1
       }
-  
-      return true;
+      else{
+          this.count=0
+      }
+      
     }
-  
+    console.log(this.average)
+    if (this.count > this.silenceCountThreshold) {
+      this.port.postMessage('silence-detected');
+      return false; // Stop processing audio when silence is detected
+    }
+
+    return true;
   }
-  
-  registerProcessor('my-audio-worklet-processor', MyAudioWorkletProcessor);
+
+}
+registerProcessor('my-audio-worklet-processor', MyAudioWorkletProcessor);
   
 `;
 
