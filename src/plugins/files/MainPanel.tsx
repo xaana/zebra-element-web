e3823ead47c2220d830dc23356e17db422d22eb9
rohit-xai <143767902+rohit-xai@users.noreply.github.com>
2024-03-06 09:26:58 +1100

    Login page modified

--- src/plugins/files/MainPanel.tsx
+++ src/plugins/files/MainPanel.tsx
@@ -1,98 +1,114 @@
 import React, { useEffect, useState } from "react";
-
-import { Filter, MatrixClient, MatrixEvent, Room } from 'matrix-js-sdk/src/matrix';
-import { columns, DataTable, File } from './DataTable';
-import DMRoomMap from 'matrix-react-sdk/src/utils/DMRoomMap';
+import { Filter, MatrixClient, MatrixEvent, Room } from "matrix-js-sdk/src/matrix";
+import DMRoomMap from "matrix-react-sdk/src/utils/DMRoomMap";
 import { MediaEventHelper } from "matrix-react-sdk/src/utils/MediaEventHelper";
-import { useMatrixClientContext } from 'matrix-react-sdk/src/contexts/MatrixClientContext';
-import { init as initRouting } from '../../vector/routing';
+import { useMatrixClientContext } from "matrix-react-sdk/src/contexts/MatrixClientContext";
+
+import { columns, DataTable, File } from "./DataTable";
+import { init as initRouting } from "../../vector/routing";
 
 const isDirectMessageRoom = (client: MatrixClient, room: Room) => {
-  let dmRoomMap = DMRoomMap.shared();
-  if (!dmRoomMap) {
-    dmRoomMap = DMRoomMap.makeShared(client);
-    dmRoomMap.start();
-  }
+    let dmRoomMap = DMRoomMap.shared();
+    if (!dmRoomMap) {
+        dmRoomMap = DMRoomMap.makeShared(client);
+        dmRoomMap.start();
+    }
 
-  const dmRoomIds = dmRoomMap.getRoomIds();
-  return dmRoomIds.has(room.roomId)
-}
+    const dmRoomIds = dmRoomMap.getRoomIds();
+    return dmRoomIds.has(room.roomId);
+};
 
 export const MainPanel = () => {
-  const [rooms, setRooms] = useState<Room[]>([]); 
-  const [events, setEvents] = useState<MatrixEvent[]>([]);
-  const client = useMatrixClientContext();
+    const [rooms, setRooms] = useState<Room[]>([]);
+    const [events, setEvents] = useState<MatrixEvent[]>([]);
+    const client = useMatrixClientContext();
 
-  useEffect(() => {
-    const fetchFileEventsServer = async (rooms: Room[]) => {
-      const dmRooms = [];
-      const commonRooms = [];
-      for (let room of rooms) {
-        if (isDirectMessageRoom(client, room)) {
-          dmRooms.push(room);
-        } else {
-          commonRooms.push(room);
-        }
-      }
-    
-      const dmFilter = new Filter(client.getSafeUserId());
-      dmFilter.setDefinition({
-        room: {
-          timeline: {
-            contains_url: true,
-            types: ["m.room.message"],
-          },
-        },
-      });
-    
-      dmFilter.filterId = await client.getOrCreateFilter("FILTER_FILES_PLAIN_" + client.credentials.userId, dmFilter);
-      const dmTimelineSets = dmRooms.map((room) => room.getOrCreateFilteredTimelineSet(dmFilter));
-      const dmEvents = dmTimelineSets.flatMap((ts) => ts.getTimelines().flatMap((t) => t.getEvents()));
-    
-      const commonFilter = new Filter(client.getSafeUserId());
-      commonFilter.setDefinition({
-        room: {
-          timeline: {
-            types: ["m.room.message"],
-          },
-        },
-      });
-    
-      commonFilter.filterId = await client.getOrCreateFilter("FILTER_FILES_ENCRYPTED_" + client.credentials.userId, commonFilter);
-      const commonTimelineSets = commonRooms.map((room) => room.getOrCreateFilteredTimelineSet(commonFilter));
-      const commonEvents = commonTimelineSets
-        .flatMap((ts) => ts.getTimelines().flatMap((t) => t.getEvents()))
-        .filter((ev) => ev.getContent().file);
+    useEffect(() => {
+        const fetchFileEventsServer = async (rooms: Room[]) => {
+            const dmRooms = [];
+            const commonRooms = [];
+            for (const room of rooms) {
+                if (isDirectMessageRoom(client, room)) {
+                    dmRooms.push(room);
+                } else {
+                    commonRooms.push(room);
+                }
+            }
 
-      setEvents([ ...dmEvents, ...commonEvents]);
-    }
+            const dmFilter = new Filter(client.getSafeUserId());
+            dmFilter.setDefinition({
+                room: {
+                    timeline: {
+                        contains_url: true,
+                        types: ["m.room.message"],
+                    },
+                },
+            });
 
-    initRouting();
+            dmFilter.filterId = await client.getOrCreateFilter(
+                "FILTER_FILES_PLAIN_" + client.credentials.userId,
+                dmFilter,
+            );
+            const dmTimelineSets = dmRooms.map((room) => room.getOrCreateFilteredTimelineSet(dmFilter));
+            const dmEvents = dmTimelineSets.flatMap((ts) => ts.getTimelines().flatMap((t) => t.getEvents()));
 
-    const newRooms = client.getVisibleRooms(false);
-    setRooms(newRooms);
+            console.log("dmTimelineSets", dmTimelineSets);
+            console.log("dmEvents", dmEvents);
 
-    fetchFileEventsServer(newRooms);
-  }, []);
+            const commonFilter = new Filter(client.getSafeUserId());
+            commonFilter.setDefinition({
+                room: {
+                    timeline: {
+                        types: ["m.room.message"],
+                    },
+                },
+            });
 
-  const files: File[] = events.map((event) => {
-    const mxcUrl = event.getContent().url?? event.getContent().file?.url;
-    return {
-      id: event.getId()?? '',
-      name: event.getContent().body,
-      downloadUrl: client.mxcUrlToHttp(mxcUrl)?? '',
-      timestamp: new Date(event.localTimestamp),
-      roomId: event.getRoomId()?? '',
-      room: rooms.find((r) => r.roomId === event.getRoomId()),
-      sender: event.getSender()?? '',
-      isEncrypted: event.isEncrypted(),
-      mediaHelper: new MediaEventHelper(event),
-    }
-  });
+            commonFilter.filterId = await client.getOrCreateFilter(
+                "FILTER_FILES_ENCRYPTED_" + client.credentials.userId,
+                commonFilter,
+            );
+            const commonTimelineSets = commonRooms.map((room) => room.getOrCreateFilteredTimelineSet(commonFilter));
+            const commonEvents = commonTimelineSets
+                .flatMap((ts) => ts.getTimelines().flatMap((t) => t.getEvents()))
+                .filter((ev) => ev.getContent().file);
 
-  return (
-    <div>
-      <DataTable columns={columns} data={files} />
-    </div>
-  );
-}
+            console.log("commonTimelineSets", commonTimelineSets);
+            console.log("commonEvents", commonEvents);
+
+            setEvents([...dmEvents, ...commonEvents]);
+        };
+
+        initRouting();
+
+        const newRooms = client.getVisibleRooms(false);
+        setRooms(newRooms);
+
+        console.log("newRooms", newRooms);
+
+        fetchFileEventsServer(newRooms);
+    }, [client]);
+
+    const files: File[] = events.map((event) => {
+        const mxcUrl = event.getContent().url ?? event.getContent().file?.url;
+        return {
+            id: event.getId() ?? "",
+            name: event.getContent().body,
+            downloadUrl: client.mxcUrlToHttp(mxcUrl) ?? "",
+            timestamp: new Date(event.localTimestamp),
+            roomId: event.getRoomId() ?? "",
+            room: rooms.find((r) => r.roomId === event.getRoomId()),
+            sender: event.getSender() ?? "",
+            isEncrypted: event.isEncrypted(),
+            mediaHelper: new MediaEventHelper(event),
+        };
+    });
+
+    console.log("Files", files);
+
+    return (
+        <div>
+            <DataTable columns={columns} data={files} />
+        </div>
+    );
+};
