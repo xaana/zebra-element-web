b2aa5d8457290de42fa032afec531e82b3704253
oliverliu23 <oliver.liu@xaana.ai>
2024-03-05 19:11:37 +1100

    feat: removing ServerPicker & footer from login page

--- src/components/views/auth/VectorAuthFooter.tsx
+++ src/components/views/auth/VectorAuthFooter.tsx
@@ -16,31 +16,31 @@ limitations under the License.
 */
 
 import React, { ReactElement } from "react";
-import SdkConfig from "matrix-react-sdk/src/SdkConfig";
+// import SdkConfig from "matrix-react-sdk/src/SdkConfig";
 
 import { _t } from "../../../languageHandler";
 
 const VectorAuthFooter = (): ReactElement => {
-    const brandingConfig = SdkConfig.getObject("branding");
-    const links = brandingConfig?.get("auth_footer_links") ?? [
-        { text: "Blog", url: "https://element.io/blog" },
-        { text: "Twitter", url: "https://twitter.com/element_hq" },
-        { text: "GitHub", url: "https://github.com/element-hq/element-web" },
-    ];
+    // const brandingConfig = SdkConfig.getObject("branding");
+    // const links = brandingConfig?.get("auth_footer_links") ?? [
+    //     { text: "Blog", url: "https://element.io/blog" },
+    //     { text: "Twitter", url: "https://twitter.com/element_hq" },
+    //     { text: "GitHub", url: "https://github.com/element-hq/element-web" },
+    // ];
 
-    const authFooterLinks: JSX.Element[] = [];
-    for (const linkEntry of links) {
-        authFooterLinks.push(
-            <a href={linkEntry.url} key={linkEntry.text} target="_blank" rel="noreferrer noopener">
-                {linkEntry.text}
-            </a>,
-        );
-    }
+    // const authFooterLinks: JSX.Element[] = [];
+    // for (const linkEntry of links) {
+    //     authFooterLinks.push(
+    //         <a href={linkEntry.url} key={linkEntry.text} target="_blank" rel="noreferrer noopener">
+    //             {linkEntry.text}
+    //         </a>,
+    //     );
+    // }
 
     return (
         <footer className="mx_AuthFooter" role="contentinfo">
-            {authFooterLinks}
-            <a href="https://matrix.org" target="_blank" rel="noreferrer noopener">
+            {/* {authFooterLinks} */}
+            <a href="https://zebra-chat-uat.algoreus.net/" target="_blank" rel="noreferrer noopener">
                 {_t("powered_by_matrix")}
             </a>
         </footer>
