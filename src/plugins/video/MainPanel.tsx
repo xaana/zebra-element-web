515b374953d0db4fdc1bd65118e50c86914274a1
Jamal Saepul Aziz <jamal.s.aziz@gmail.com>
2024-03-18 17:25:58 +0700

    Pass matrix credentials to Zebra Eye

--- src/plugins/video/MainPanel.tsx
+++ src/plugins/video/MainPanel.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getVectorConfig } from "../../vector/getconfig";
 import { Loader } from '../../components/ui/loader';
 import { useMatrixClientContext } from 'matrix-react-sdk/src/contexts/MatrixClientContext';
+import { IMatrixClientCreateOpts } from 'matrix-js-sdk/src/client';
 
 export const MainPanel = () => {
   const [config, setConfig] = useState<Record<string, any>>();
@@ -25,9 +26,19 @@ export const MainPanel = () => {
     return <Loader />
   }
 
+  const creds: IMatrixClientCreateOpts = {
+    baseUrl: client.getHomeserverUrl(),
+    idBaseUrl: client.getIdentityServerUrl(),
+    accessToken: client.getAccessToken()?? '',
+    refreshToken: client.getRefreshToken()?? '',
+    userId: client.getUserId()?? '',
+    deviceId: client.getDeviceId()?? '',
+  };
+
+  const credsStr = window.btoa(JSON.stringify(creds));
+
   const url = new URL(config.url);
-  url.searchParams.set('userId', client.getUserId()?? '');
-  url.searchParams.set('accessToken', client.getAccessToken()?? '');
+  url.searchParams.set('creds', credsStr);
       
   return (
     <iframe src={url.toString()} style={{ height: "100%", width: "100%", border: "none" }}>
