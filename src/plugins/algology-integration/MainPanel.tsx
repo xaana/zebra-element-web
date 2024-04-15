e106c78b08600c1a11e50fb5b3fe2c46b9ff2c78
oliverliu23 <oliver.liu@xaana.ai>
2024-04-15 18:17:02 +1000

    feat: supporting in-app redirecting/routing with alert card

--- src/plugins/algology-integration/MainPanel.tsx
+++ src/plugins/algology-integration/MainPanel.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 // import { Direction, Filter, MatrixEvent, Room } from "matrix-js-sdk/src/matrix";
 import { useMatrixClientContext } from "matrix-react-sdk/src/contexts/MatrixClientContext";
 
-import { getVectorConfig } from "@/vector/getconfig";
+import { IExtendedConfigOptions, getVectorConfig } from "@/vector/getconfig";
 // import * as jose from jose;
 
 // TODO: Add extra generation logic for generating this token
@@ -12,22 +12,24 @@ const STATIC_JWT_TOKEN =
     "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiJ9.N3Depdc6Amt7EoRB4k0t9RfIHKv79YDUvaKy6b5wIiGLYIPx3AhaY7gyc9Fuvgy_jSa9_-gPeLYiEltIzzm-E1HWQh7lIchq-t2v_oAMvAfjE0gS6yETdhKJ-cyDmVJBwI9H9G1SbVVIiE3c_yJRPpSe_qsoVwDYisGvoQSM4WDUlZVJ0h32UetB-eAL8hewmmxN4b3dTaMdvApneGOpIMBGnkY9aQOlmb0Ym6kh31COYTtxdhrPWVS6EA7r_lBIMXW1efY06igWTlrR1O0o7LaY4E4dwg-gXxIyGpsf8ifxZmwsLd_3_y_8e3wRycf_tTYZjJAofKdLCxka_-3uMA";
 
 export const MainPanel = (): JSX.Element => {
-    // const [rooms, setRooms] = useState<Room[]>([]);
-    // const [events, setEvents] = useState<MatrixEvent[]>([]);
     const [iframeUrl, setIframeUrl] = useState<string | undefined>(undefined);
+    const [specificUri, setSpecificUri] = useState("");
     const [token, setToken] = useState("");
     const client = useMatrixClientContext();
     useEffect(() => {
-        fetch("https://dev-api-2.securezebra.com/api/algology_login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({user_id: client.getUserId()})
+        let configData: IExtendedConfigOptions | undefined;
+        getVectorConfig().then((config)=>{
+            configData = config;
+            return fetch(config?.plugins.reports.api + "/api/algology_login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({user_id: client.getUserId()})
+            })
         })
         .then(res=> res.json())
         .then(async res=>{
-            const configData = await getVectorConfig();
             if (configData?.plugins["reports"]) {
                 setIframeUrl(configData?.plugins["algology"].url);
             }
@@ -38,7 +40,8 @@ export const MainPanel = (): JSX.Element => {
             setIframeUrl("http://localhost:3001")
             setToken(STATIC_JWT_TOKEN);
         })
-        
+        setSpecificUri(localStorage.getItem("pluginUri") || "")
+        localStorage.removeItem("pluginUri")
         // Simulating lag
         // setTimeout(() => {
         //     setToken(STATIC_JWT_TOKEN);
@@ -52,8 +55,7 @@ export const MainPanel = (): JSX.Element => {
                 <iframe
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     id="algology-iframe"
-                    src={`${iframeUrl}/?orgId=1&kiosk=tv&auth_token=${token}`}
-                    frameBorder="0"
+                    src={`${iframeUrl}/${specificUri?specificUri:"?"}kiosk=tv&auth_token=${token}`}
                     height="100%"
                     width="100%"
                 />
