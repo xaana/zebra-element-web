c60a9588e0daea5ed2a9b206af80c6f0ead26610
oliverliu23 <oliver.liu@xaana.ai>
2024-05-13 02:32:13 +1000

    feat: AB#1676 adjusting response format for websearch and database query

--- src/components/ui/databasePill.tsx
+++ src/components/ui/databasePill.tsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import { TimelineRenderingType } from 'matrix-react-sdk/src/contexts/RoomContext';
+import React from "react";
+import { TimelineRenderingType } from "matrix-react-sdk/src/contexts/RoomContext";
 import dis from "matrix-react-sdk/src/dispatcher/dispatcher";
-import { X } from 'lucide-react';
+import { X } from "lucide-react";
 
-import { IconDatabase } from './icons';
+import { IconDatabase } from "./icons";
 
 // import { IconDatabase } from './icons';
 
-
-
-
 // Define a functional component
-const DatabasePill = ({ database,timelineRenderingType,roomId}:{database:string|undefined,timelineRenderingType:TimelineRenderingType,roomId?:string}) : React.JSX.Element | null =>  {
-  // Styles for the pill container
-  const cancelQuoting=(): void =>  {
-    dis.dispatch({
-        action: "select_database",
-        database: "",
-        roomId: roomId,
-        context:timelineRenderingType,
-    });
-}
+const DatabasePill = ({
+    database,
+    timelineRenderingType,
+    roomId,
+}: {
+    database: string | undefined;
+    timelineRenderingType?: TimelineRenderingType;
+    roomId?: string;
+}): React.JSX.Element | null => {
+    // Styles for the pill container
+    const cancelQuoting = (): void => {
+        dis.dispatch({
+            action: "select_database",
+            database: "",
+            roomId: roomId,
+            context: timelineRenderingType,
+        });
+    };
 
-  if (!database) return null;
-  return (
-    <div className="inline-block px-2 bg-muted rounded-full gap-2">
-        <div className='flex flex-row text-xs'>
-        <IconDatabase />
-      <span>{database}</span>
+    if (!database) return null;
+    return (
+        <div className="inline-block px-2 bg-muted rounded-full gap-2">
+            <div className="flex flex-row text-xs">
+                <IconDatabase />
+                <span>{database}</span>
 
-      <X onClick={()=>cancelQuoting()} className='cursor-pointer' size={16} />
-
-      </div>
-    </div>
-  );
+            { (timelineRenderingType || roomId) && <X onClick={() => cancelQuoting()} className="cursor-pointer" size={16} />}
+            </div>
+        </div>
+    );
 };
 
 export default DatabasePill;
