425bfad205f612538dcb7608e9e32f74f871b302
tomzoux <tom.zou@xaana.ai>
2024-05-15 16:05:47 +1000

    Update Icons in SpacePanel and MessageComposer

--- src/plugins/plugin.ts
+++ src/plugins/plugin.ts
@@ -3,7 +3,7 @@ import React from "react";
 export interface Plugin {
     name: string;
     label?: string;
-    icon: React.FC<React.SVGProps<SVGSVGElement> & { className?: string; fill?: string }>;
+    Icon: React.FC;
     MainPanel: React.FC;
     LeftPanel?: React.FC;
 }
--- src/plugins/reports/index.tsx
+++ src/plugins/reports/index.tsx
@@ -1,11 +1,10 @@
 import { Plugin } from "../plugin";
 import { MainPanel } from "./MainPanel";
-
-import { IconFileChartFill } from "@/components/ui/icons";
+import { IconFileChart } from "@/components/ui/icons";
 
 export const ReportsPlugin: Plugin = {
     name: "reports",
     label: "Reports",
-    icon: IconFileChartFill,
+    Icon: IconFileChart,
     MainPanel: MainPanel,
 };
