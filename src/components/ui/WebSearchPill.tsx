d904e76da63fcc276a5111ee833daa284ff9d9d4
tomzoux <tom.zou@xaana.ai>
2024-05-17 15:52:07 +1000

    Update icons

--- src/components/ui/WebSearchPill.tsx
+++ src/components/ui/WebSearchPill.tsx
@@ -1,34 +1,28 @@
-import React from 'react';
-import { Earth} from 'lucide-react';
-
-
-
-
-
+import React from "react";
+import { Earth } from "lucide-react";
 
 // Define a functional component
-const WebSearchPill = () : React.JSX.Element | null =>  {
-  // Styles for the pill container
-//   const cancelQuoting=(): void =>  {
-//     dis.dispatch({
-//         action: "select_database",
-//         database: "",
-//         roomId: roomId,
-//         context:timelineRenderingType,
-//     });
-// }
+const WebSearchPill = (): React.JSX.Element | null => {
+    // Styles for the pill container
+    //   const cancelQuoting=(): void =>  {
+    //     dis.dispatch({
+    //         action: "select_database",
+    //         database: "",
+    //         roomId: roomId,
+    //         context:timelineRenderingType,
+    //     });
+    // }
 
-  return (
-    <div className="inline-block px-2 bg-muted rounded-full gap-2">
-        <div className='flex flex-row text-xs'>
-        <Earth size={16} />
-      <span>Web Search</span>
+    return (
+        <div className="inline-block px-2 bg-muted rounded-full gap-2">
+            <div className="flex flex-row text-xs">
+                <Earth size={16} />
+                <span>Web Search</span>
 
-      {/* <X onClick={()=>cancelQuoting()} className='cursor-pointer' size={16} /> */}
-
-      </div>
-    </div>
-  );
+                {/* <X onClick={()=>cancelQuoting()} className='cursor-pointer' size={16} /> */}
+            </div>
+        </div>
+    );
 };
 
 export default WebSearchPill;
