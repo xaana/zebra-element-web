c2965291e9c66b99046b30aa18ca68830ef394a7
XinjieJayWang <jay.wang@xaana.ai>
2024-06-17 13:45:41 +1000

    Update streaming for pdf

--- src/components/views/messages/ZebraStream.tsx
+++ src/components/views/messages/ZebraStream.tsx
@@ -5,89 +5,139 @@ import { WebSearchSourceItem, WebSearchSources } from '@/components/web/WebSearc
 import { Separator } from '@/components/ui/separator';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Bell } from 'lucide-react';
+import { DocFile } from '../rooms/FileSelector';
+import { IContent } from 'matrix-js-sdk/src/matrix';
 
 interface IProps {
-  fetching?: boolean;
   roomId?: string;
   eventId?: string;
-  type?: string;
   rawQuestion?: string;
-  questionId?: string;
+  content: IContent
 }
 
-const ZebraStream: React.FC<IProps> = ({ fetching, roomId, eventId, type,rawQuestion,questionId }) => { 
+const ZebraStream: React.FC<IProps> = ({ roomId, eventId,rawQuestion,content }) => { 
     const [markdown, setMarkdown] = useState("");
     const [webSource, setWebSource] = useState<WebSearchSourceItem[]>([]);
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const payload = {
-                    roomId: roomId,
-                    firstMessageEventId: eventId,
-                    question: rawQuestion,
-                    eventId:questionId,
-                    
-                };
-                const url = `${SettingsStore.getValue("botApiUrl")}/web_stream`;
-                const request = new Request(url, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(payload),
-                });
-                const response = await fetch(request);
-                
-                // Check if response.body is not null
-                if (response.body) {
-                    const reader = response.body.getReader();
-                    const decoder = new TextDecoder('utf-8');
-                    let firstChunk = true;
-                    const readStream = async () => {
-                        const { done, value } = await reader.read();
-                        if (done) {
-                            console.log("Stream complete");
-                            return;
-                        }
-                        // Decode the stream chunk to text
-                        const chunk = decoder.decode(value, { stream: true });
-                        // console.log("Stream chunk:", JSON.parse(chunk));
-                        if (firstChunk) {
-                            firstChunk = false;
-                            const webCitations: WebSearchSourceItem[] = JSON.parse(chunk).source_links.map((item: string) => {
-                                const url = new URL(item);
-                                return {
-                                    link: item,
-                                    hostname: url.hostname,
-                                };
-                            });
-                            if(JSON.parse(chunk).content) {setMarkdown(JSON.parse(chunk).content);}
-                            setWebSource(webCitations);
-                        }else{
-                            setMarkdown(chunk);
-                        }
-                        
+        
 
-                        // Read the next chunk
-                        await readStream();
-                    };
-
-                    await readStream();
-                } else {
-                    console.error('Response body is null.');
-                }
-            } catch (error) {
-                console.error('Failed to fetch data:', error);
-            }
-        };
-
-        fetchData();
+        if(content.type === "web")fetchWebStream();
+        if (content.type === "pdf")fetchPdfStream();
 
         return () => {
             // Cleanup logic if needed, e.g., aborting the fetch
         };
 
     }, []);
+    const fetchWebStream = async () => {
+        try {
+            const payload = {
+                roomId: roomId,
+                firstMessageEventId: eventId,
+                question: rawQuestion,
+                eventId:content.questionId,
+                
+            };
+            const url = `${SettingsStore.getValue("botApiUrl")}/web_stream`;
+            const request = new Request(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(payload),
+            });
+            const response = await fetch(request);
+            
+            // Check if response.body is not null
+            if (response.body) {
+                const reader = response.body.getReader();
+                const decoder = new TextDecoder('utf-8');
+                let firstChunk = true;
+                const readStream = async () => {
+                    const { done, value } = await reader.read();
+                    if (done) {
+                        console.log("Stream complete");
+                        return;
+                    }
+                    // Decode the stream chunk to text
+                    const chunk = decoder.decode(value, { stream: true });
+                    // console.log("Stream chunk:", JSON.parse(chunk));
+                    if (firstChunk) {
+                        firstChunk = false;
+                        const webCitations: WebSearchSourceItem[] = JSON.parse(chunk).source_links.map((item: string) => {
+                            const url = new URL(item);
+                            return {
+                                link: item,
+                                hostname: url.hostname,
+                            };
+                        });
+                        if(JSON.parse(chunk).content) {setMarkdown(JSON.parse(chunk).content);}
+                        setWebSource(webCitations);
+                    }else{
+                        setMarkdown(chunk);
+                    }
+                    
+
+                    // Read the next chunk
+                    await readStream();
+                };
+
+                await readStream();
+            } else {
+                console.error('Response body is null.');
+            }
+        } catch (error) {
+            console.error('Failed to fetch data:', error);
+        }
+    };
+    const fetchPdfStream = async () => {
+        try {
+            const payload = {
+                roomId: roomId,
+                firstMessageEventId: eventId,
+                question: rawQuestion,
+                eventId:content.questionId,
+                mediaIds: content.fileSelected,
+                webFlag: content.webFlag,
+                pdfSummary: content.pdfSummary
+                
+            };
+            const url = `${SettingsStore.getValue("botApiUrl")}/pdf_query_stream`;
+            const request = new Request(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(payload),
+            });
+            const response = await fetch(request);
+            
+            // Check if response.body is not null
+            if (response.body) {
+                const reader = response.body.getReader();
+                const decoder = new TextDecoder('utf-8');
+                const readStream = async () => {
+                    const { done, value } = await reader.read();
+                    if (done) {
+                        console.log("Stream complete");
+                        return;
+                    }
+                    // Decode the stream chunk to text
+                    const chunk = decoder.decode(value, { stream: true });
+                    // console.log("Stream chunk:", JSON.parse(chunk));
+                    setMarkdown(chunk);
+                    // Read the next chunk
+                    await readStream();
+                };
+
+                await readStream();
+            } else {
+                console.error('Response body is null.');
+            }
+        } catch (error) {
+            console.error('Failed to fetch data:', error);
+        }
+    }
 
     const getSectionTitle = (title: string, Icon: React.FC | null): React.ReactNode => {
         return (
@@ -98,8 +148,8 @@ const ZebraStream: React.FC<IProps> = ({ fetching, roomId, eventId, type,rawQues
         );
     }
 
-    if(!fetching) return null
-    if (webSource.length === 0) return (<div>Thinking...</div>)
+    if(!content.fetching) return null
+    if (markdown === "") return (<div>Thinking...</div>)
 
     return (
         <div>
