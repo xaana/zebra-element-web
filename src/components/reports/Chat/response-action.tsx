4354dbd7ebf822c83e19abfe4dd680feecc83a3f
rohit-xai <143767902+rohit-xai@users.noreply.github.com>
2024-07-05 14:44:19 +1000

    Update response-action.tsx

--- src/components/reports/Chat/response-action.tsx
+++ src/components/reports/Chat/response-action.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { Editor } from "@tiptap/react";
 
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
