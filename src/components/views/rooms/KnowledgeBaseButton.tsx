de0ed4f32aea83b52833d497bcf0aba68990c694
XinjieJayWang <jay.wang@xaana.ai>
2024-07-05 14:22:14 +1000

    Fixed the option menu close issue

--- src/components/views/rooms/KnowledgeBaseButton.tsx
+++ src/components/views/rooms/KnowledgeBaseButton.tsx
@@ -2,11 +2,10 @@ import { CollapsibleButton } from "matrix-react-sdk/src/components/views/rooms/C
 import RoomContext from "matrix-react-sdk/src/contexts/RoomContext";
 import dis from "matrix-react-sdk/src/dispatcher/dispatcher";
 import React, { useContext } from "react";
-import { OverflowMenuContext } from "./MessageComposerButtons";
 import { Action } from "matrix-react-sdk/src/dispatcher/actions";
 
 
-export const KnowledgeBaseButton = (): JSX.Element => {
+export const KnowledgeBaseButton = ({toggleButtonMenu}:{toggleButtonMenu: () => void}): JSX.Element => {
     const { roomId, timelineRenderingType } = useContext(RoomContext);
     const handleClick = () => {
         dis.dispatch({
@@ -19,13 +18,14 @@ export const KnowledgeBaseButton = (): JSX.Element => {
             action: Action.FocusAComposer,
             context: timelineRenderingType,
         });
+        toggleButtonMenu();
     };
     
 
     return (
         <CollapsibleButton
             style={{ color: "#7B6AE0" }}
-            title="Knowledge Base"
+            title="Ziggy"
             className="mx_MessageComposer_button"
             iconClassName="knowledge_button"
             onClick={handleClick}
--- src/components/views/rooms/MessageComposerButtons.tsx
+++ src/components/views/rooms/MessageComposerButtons.tsx
@@ -140,7 +140,7 @@ const MessageComposerButtons: React.FC<IProps> = (props: IProps) => {
             uploadButton(),
             databaseSelector(),
             filesSelector(room.roomId),
-            knowledgeBaseButton(room.roomId),
+            knowledgeBaseButton(room.roomId,props.toggleButtonMenu),
             voiceRecordingButton(props, narrow),
             audioCaptureButton(),
             // voiceBotButton(matrixClient, room),
@@ -225,8 +225,8 @@ function filesSelector(roomId: string): ReactElement {
     return <FileSelector key="controls_filesSelector" roomId={roomId} />;
 }
 
-function knowledgeBaseButton(roomId: string): ReactElement {
-    return <KnowledgeBaseButton key="KnowledgeBaseButton" />;
+function knowledgeBaseButton(roomId: string, toggleButtonMenu: () => void): ReactElement {
+    return <KnowledgeBaseButton key="KnowledgeBaseButton" toggleButtonMenu={toggleButtonMenu} />;
 }
 
 function voiceBotButton(matrixClient: MatrixClient, room: Room): ReactElement {
