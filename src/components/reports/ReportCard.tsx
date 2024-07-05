9c34da0c0096fb536701254ed582560ef492dc43
rohit-xai <143767902+rohit-xai@users.noreply.github.com>
2024-07-05 11:49:23 +1000

    Updated listing of documents based on type

--- src/components/reports/ReportCard.tsx
+++ src/components/reports/ReportCard.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { formatDistanceToNow } from "date-fns";
 
-import { ReportThumbnail } from "./ReportThumbnail";
+import { DocumentThumbnail } from "./DocumentThumbnail";
 import type { Report } from "@/plugins/reports/types";
 import { ReportActions } from "./ReportActions";
+import { SpreadsheetThumbnail } from "./SpreadsheetThumbnail";
+import { IconDocumentExcel, IconDocumentWord } from "../ui/icons";
 
-// import { Badge } from "@/components/ui/badge";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 
 export const ReportCard = ({
     report,
@@ -26,22 +29,31 @@ export const ReportCard = ({
 }): JSX.Element => {
     return (
         <div
-            className="rounded-lg relative flex flex-col border bg-background text-left text-sm transition-all hover:bg-card overflow-hidden cursor-pointer outline-none"
+            className={cn(
+                "rounded-lg relative flex flex-col border bg-background text-left text-sm transition-all hover:bg-card overflow-hidden cursor-pointer outline-none",
+            )}
             onClick={async () => await onSelectReport(report)}
         >
             <div className="w-full aspect-video relative shrink-0">
-                <ReportThumbnail />
+                <Badge
+                    className={cn(
+                        "absolute bottom-0 right-0 m-2 uppercase tracking-wide text-[10px] leading-none p-1",
+                        // report.fileType === "docx" ? "bg-[#D7E9FF] text-[#2d74be]" : "bg-[#CFE3D7] text-[#3c9063]",
+                        report.fileType === "docx" ? "bg-white text-[#2d74be]" : "bg-white text-[#3c9063]",
+                    )}
+                    variant="secondary"
+                >
+                    {report.fileType === "docx" ? (
+                        <IconDocumentWord className="w-3 h-3 mr-1" />
+                    ) : (
+                        <IconDocumentExcel className="w-3 h-3 mr-1" />
+                    )}
+                    {report.fileType === "docx" ? "Document" : "Spreadsheet"}
+                </Badge>
+                {report.fileType === "docx" ? <DocumentThumbnail /> : <SpreadsheetThumbnail />}
             </div>
             <div className="p-3 flex-1 flex w-full flex-col justify-between gap-2 relative">
-                <div className="font-semibold">{`${report.name}.${report.fileType}`}</div>
-                {/* <div>
-                    <Badge
-                        className="uppercase tracking-wide text-[10px] leading-none p-1 text-muted-foreground"
-                        variant="secondary"
-                    >
-                        {report.owner === userId ? "mine" : "shared"}
-                    </Badge>
-                </div> */}
+                <div className="font-semibold">{report.name}</div>
                 <div className="text-[10px] text-muted-foreground">
                     {Number(report.id) < 0
                         ? "Preset report"
--- src/components/reports/ReportsList.tsx
+++ src/components/reports/ReportsList.tsx
@@ -11,6 +11,7 @@ import {
 } from "@tanstack/react-table";
 import { format, parseISO, isToday } from "date-fns";
 
+import { IconDocumentExcel, IconDocumentWord } from "@/components/ui/icons";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Icon } from "@/components/ui/Icon";
@@ -52,8 +53,13 @@ export const ReportsList = ({
                 );
             },
             cell: ({ row }) => (
-                <Button onClick={onSelectReport.bind(null, row.original)} variant="link">
-                    {`${row.getValue("name")}.${row.original.fileType}`}
+                <Button onClick={onSelectReport.bind(null, row.original)} variant="link" className="text-foreground">
+                    {row.original.fileType === "docx" ? (
+                        <IconDocumentWord className="w-5 h-5 mr-2 text-[#2d74be]" />
+                    ) : (
+                        <IconDocumentExcel className="w-5 h-5 mr-2 text-[#3c9063]" />
+                    )}
+                    {`${row.getValue("name")}`}
                 </Button>
             ),
         },
